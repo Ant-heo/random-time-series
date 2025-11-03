48e0078371d59f47933378eddaa8c20146b9d31e
Anthéo <antheo.reynaud@gmail.com>
2025-11-03 21:20:27 +0100

    distances calculation

--- appleData.py
+++ appleData.py
@@ -30,7 +30,7 @@ def load_generated_time_series(path):
     ts = ts.iloc[:, :-21]
     return [ts.loc[i].tolist() for i in range(len(ts))]
 
-def compare_series(ref_series, ts_series, output_dir):
+def compare_series(ref_series, ts_series, output_dir, save_plots=False):
     ref_norm = normalize_series(ref_series)
     ts_norm = [normalize_series(ts_i) for ts_i in ts_series]
     
@@ -39,13 +39,14 @@ def compare_series(ref_series, ts_series, output_dir):
 
     distances = []
     for i in range(len(ts_norm)):
-        print(f"\n=== Time Series {i} ===")
         distances.append(compute_dtw_matrix(ref_norm, ts_norm[i])[-1][-1])
-        plot_time_series(ref_norm, ts_norm[i], title=f'{i} Apple Series Comparison', save_dir=output_dir)
-        plot_dtw_path(ref_norm, ts_norm[i], title=f'{i} DTW Matrix Standard', save_dir=output_dir)
-        plot_dtw_path_restricted(ref_norm, ts_norm[i], 20, title=f'{i} DTW Matrix Restricted', save_dir=output_dir)
-        plot_dtw_alignment(ref_norm, ts_norm[i], 'standard', title=f'{i} DTW Alignment Standard', save_dir=output_dir)
-        plot_dtw_alignment(ref_norm, ts_norm[i], 'restricted', window=20, title=f'{i} DTW Alignment Restricted', save_dir=output_dir)
+        if save_plots:
+            print(f"\nPlotting Time Series {i}")
+            plot_time_series(ref_norm, ts_norm[i], title=f'{i} Apple Series Comparison', save_dir=output_dir)
+            plot_dtw_path(ref_norm, ts_norm[i], title=f'{i} DTW Matrix Standard', save_dir=output_dir)
+            plot_dtw_path_restricted(ref_norm, ts_norm[i], 20, title=f'{i} DTW Matrix Restricted', save_dir=output_dir)
+            plot_dtw_alignment(ref_norm, ts_norm[i], 'standard', title=f'{i} DTW Alignment Standard', save_dir=output_dir)
+            plot_dtw_alignment(ref_norm, ts_norm[i], 'restricted', window=20, title=f'{i} DTW Alignment Restricted', save_dir=output_dir)
     
     return distances
 
@@ -54,3 +55,6 @@ ref = load_apple_reference_data()
 
 distances = compare_series(ref, ts, output_dir)
 print("DTW Distances:", distances)
+print("Average DTW Distance:", sum(distances) / len(distances))
+print("Minimum DTW Distance:", min(distances))
+print("Maximum DTW Distance:", max(distances))
